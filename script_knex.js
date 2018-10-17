6ff25e1a146e3971322e05339524c6a044afab6a
Andrea <andreamayhirji@gmail.com>
2018-10-17 00:44:40 +0000

    invoked the famousPeople function

--- script_knex.js
+++ script_knex.js
@@ -20,40 +20,8 @@ function famousPeople(array) {
     });
 }
 
-//-------------------- CONNECTION USING SQL --------------------//
-// client.connect((err) => {
-//     if (err) {
-//         return console.error("Connection Error", err);
-//     }
-//     client.query(`SELECT * FROM famous_people WHERE first_name = $1`, [process.argv[2]], (err, results) => {
-//         if (err) {
-//             return console.error("error running query", err);
-//         }
-//         let person = results.rows;
-//         console.log(`Found ${person.length} person(s) by the name '${process.argv[2]}':`)
-//         //callback from famousPeople function and using results.rows as the paramater to loop through.
-//         famousPeople(results.rows);
-//         client.end();
-
-//     });
-// });
 
 //-------------------- CONNECTION USING KNEX --------------------//
-
-
-/* using .then and promise INSTEAD of a callback */
-/* must have a .catch at the end, then we can keep chaining using the result from what .then ran */
-
-// knex
-//     .select('*')
-//     .from('famous_people')
-//     .then(function(rows) {
-//         console.log(rows)
-//     })
-//     .catch(function(err) {
-//         console.log("ERROR",err)
-//     })
-
 
 knex
     .from('famous_people')
@@ -63,5 +31,21 @@ knex
     .select()
     .asCallback(function (err, rows) {
         if (err) return console.error(err);
-        console.log(rows);
+        famousPeople(rows)
     });
+
+/* using .then and promise INSTEAD of a callback */
+/* must have a .catch at the end, then we can keep chaining using the result from what .then ran */
+
+// knex
+//     .from('famous_people')
+//     .where({
+//         first_name: process.argv[2]
+//     })
+//     .select()
+//     .then(function (rows) {
+//         console.log(famousPeople(rows))
+//     })
+//     .catch(function (err) {
+//         console.log("ERROR", err)
+//     });
